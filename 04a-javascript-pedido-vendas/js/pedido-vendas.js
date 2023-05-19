639fdcdcda2561b9b2420aa25dc1b241e07c2c16
Joao Carlos <jcpinheiro@ifma.edu.br>
2023-05-19 08:52:04 -0300

    aula javascript 19-05-2023

---
+++ 04a-javascript-pedido-vendas/js/pedido-vendas.js
@@ -0,0 +1,14 @@
+//  let nome = prompt('Digite o seu nome')
+//  alert('Bom dia ' + nome) 
+
+let inputNome = document.querySelector('#nome')
+let inputQuantidade = document.querySelector('#quantidade')
+let inputValor = document.querySelector('#valor')
+
+
+console.log(inputNome.value)
+console.log(inputQuantidade.value)
+console.log(inputValor.value)
+
+
+
